2aac2b50ab21a0c2cb65ac50325331991f0c00c5
Tomo Watanabe <tomovwgti@gmail.com>
2012-08-15 02:28:57 +0900

    Added AirConSocketIO

---
+++ AirConSocketIO/src/com/tomovwgti/json/Msg.java
@@ -0,0 +1,49 @@
+package com.tomovwgti.json;
+
+public class Msg {
+    String sender;
+    String command;
+    String message;
+    String temperature;
+    int setting;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getSetting() {
+        return setting;
+    }
+
+    public void setSetting(int setting) {
+        this.setting = setting;
+    }
+}
